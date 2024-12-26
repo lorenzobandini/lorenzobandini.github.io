6b648bdcd785193b68cd3fe846e58406bdcaf28b
lorenzo <lboa@hotmail.it>
2024-12-26 22:46:57 +0100

    install shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
